aaf6ccb65de0e1c0f5afe3a70bb153d9ffb22a83
Robbinsonke <robbinsonmuthike@gmail.com>
2023-10-05 02:36:52 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program without using the function puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
